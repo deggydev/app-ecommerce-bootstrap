0974d47b783b0bb95c61d2c861510dde3dbe90f7
deguidev <dlp.diegolipa@gmail.com>
2026-04-22 14:49:04 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './style.css'
+
+import { productos } from './data/producto.js'
+import { mostrarHero, mostrarCatalogo } from './components/catalogo.js'
+
+function inicializarApp(){
+  document.querySelector('#app').innerHTML = `
+    ${ mostrarHero() }
+    ${ mostrarCatalogo() }
+  `
+}
+
+inicializarApp();
+
